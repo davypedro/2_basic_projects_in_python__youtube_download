116e5f4ed70153a0edee17d3fa8a2d13df0b4129
Davy Pedro <88987986+davypedro@users.noreply.github.com>
2021-12-13 04:54:03 -0300

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -1,17 +1,10 @@
 #Create by: Davy Pedro
 
 from pytube import YouTube 
-'''
-first install the pytube package. 
-to install, just type "pip install pytube" in cmd as administrator'''
-
-#library responsible for showing the progress of a given task on the terminal
 from pytube.cli import on_progress 
 import os
 
-#variable to select download option
 operation = int(input('To download video, press (1) | To download audio, press (2): '))
-
 
 if operation == 1:
     link = input('insert video link (YouTube only): ') 
@@ -24,7 +17,7 @@ if operation == 1:
 
     #function to get the highest video resolution
     videos = yt.streams.get_highest_resolution()
-    videos.download(locale) #passing location to download function
+    videos.download(locale)
 
     print('Download done successfully!')
 
@@ -37,14 +30,10 @@ else:
     print('Audio size = ', yt.length, 'seconds')
     print('Download in progress...')
 
-    '''
-    different from the first case, where we select the highest resolution, 
-    select the get_audio_only() function to get only the audio from the video. 
-    However, the video screen is black. We need to deal with this.'''
     videos = yt.streams.get_audio_only()
     out_file = videos.download(locale)
 
-    base, ext = os.path.splitext(out_file) #renaming and changing file extension to mp3
+    base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
 
